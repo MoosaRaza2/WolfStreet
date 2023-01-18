e27b6738f35dc8094770625f3ea5f6b54673dca5
MoosaRaza2 <rmoosa276@gmail.com>
2023-01-18 08:59:07 +0500

    feat: :sparkles: initial release

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    // slick slider js   =============;
+    $('.img-slider').slick({
+        // autoplay: true,
+        arrows: false,
+        // fade: true,
+        asNavFor: '.text-slider',
+    });
+    $('.text-slider').slick({
+        // autoplay: true,
+        prevArrow: '<i class="fas fa-arrow-left slick-prev left-arrow"></i>',
+        nextArrow: '<i class="fas fa-arrow-right slick-next right-arrow"></i>',
+        asNavFor: '.img-slider',
+    });
+    $('.gallery-slider').slick({
+        // autoplay: true,
+        prevArrow: '<i class="fas fa-arrow-left slick-prev left-arrow"></i>',
+        nextArrow: '<i class="fas fa-arrow-right slick-next right-arrow"></i>',
+        // asNavFor: '.img-slider',
+    });
+    $('.project-slider').slick({
+        // autoplay: true,
+        prevArrow: '<i class="fas fa-arrow-left slick-prev left-arrow"></i>',
+        nextArrow: '<i class="fas fa-arrow-right slick-next right-arrow"></i>',
+        // asNavFor: '.img-slider',
+    });
+    
+});
